8f66a6827c8acc5ed81a56db1755dd339b4b5ed1
Kuba <kubaglowacki2@gmail.com>
2022-06-05 11:50:06 +0200

    Zadanie bazy danych 1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv, sqlite3
 if __name__ == '__main__':
     con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
     curs = con.cursor()
-    cur.execute("CREATE TABLE polaczenia (from_subscriber,to_subscriber,datetime,duration,celltower);")
+    curs.execute("CREATE TABLE polaczenia (from_subscriber,to_subscriber,datetime,duration,celltower);")
 
     with open("polaczenia_duze.csv", "r") as fin:
         reader = csv.reader(fin, delimiter = ";")
