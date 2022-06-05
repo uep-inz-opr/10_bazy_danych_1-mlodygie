c44f5805ae69a9103035511f801a150c369a6d8a
Kuba <kubaglowacki2@gmail.com>
2022-06-05 11:00:43 +0200

    Zadanie bazy danych 1

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import csv, sqlite3
 
-if __name__ == "__main__":
-    pass
+if __name__ == '__main__':
+    con = sqlite3.connect(":memory:")
+    curs = con.cursor()
+    cur.execute("CREATE TABLE polaczenia (from_subscriber,to_subscriber,datetime,duration,celltower);")
+
+    with open("polaczenia_duze.csv", "r") as fin:
+        reader = csv.reader(fin, delimiter = ";")
+        next(reader, None)
+        rows = [x for x in reader]
+        curs.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?)", rows,)
+
+    sql_con
+    cursor = sql_con.cursor()
+    cursor.execute("SELECT sum(duration) from polaczenia")
+
+print(str(cursor.fetchone()))
